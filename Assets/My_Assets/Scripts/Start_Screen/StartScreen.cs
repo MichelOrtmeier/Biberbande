efbc7d9508286800a06537560e2eb2edb603c25c
Morti <morti.mo@outlook.de>
2023-12-23 14:30:57 +0100

    Added ability in startscreen to modify the playerAmount

--- Assets/My_Assets/Scripts/Start_Screen/StartScreen.cs
+++ Assets/My_Assets/Scripts/Start_Screen/StartScreen.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Button startButton;
+    [Header("Player Amount")]
+    [SerializeField] TextMeshProUGUI playerAmountText;
+    [SerializeField] Button addPlayerButton;
+    [SerializeField] Button removePlayerButton;
+    [Range(2, 6)]
+    [SerializeField] int defaultPlayerAmount;
+
+    int playerAmount;
+    const int minPlayerAmount = 2;
+    const int maxPlayerAmount = 6;
+
+    private void Start()
     {
-        
+        playerAmount = defaultPlayerAmount;
+        ShowPlayerAmount();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnStartButtonClick()
     {
-        
+
+    }
+
+    public void OnAddPlayerButtonClick()
+    {
+        if(playerAmount < maxPlayerAmount)
+        {
+            playerAmount++;
+            ShowPlayerAmount();
+        }
+    }
+
+    public void OnRemovePlayerButtonClick()
+    {
+        if(playerAmount > minPlayerAmount)
+        {
+            playerAmount--;
+            ShowPlayerAmount();
+        }
+    }
+
+    private void ShowPlayerAmount()
+    {
+        playerAmountText.text = "Spieleranzahl:" + playerAmount.ToString();
     }
 }
